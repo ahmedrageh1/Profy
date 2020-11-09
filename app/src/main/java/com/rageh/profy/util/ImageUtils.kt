353d477e36222eb011c16c3015d011057d96b568
Rageh <ahmed.rageh@passapp.com>
2020-11-09 21:30:03 +0200

    -add capture image intent dispatcher to image utils

--- app/src/main/java/com/rageh/profy/util/ImageUtils.kt
+++ app/src/main/java/com/rageh/profy/util/ImageUtils.kt
@@ -3,7 +3,11 @@ package com.rageh.profy.util
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
+import android.provider.MediaStore
+import androidx.fragment.app.Fragment
 
+
+const val REQUEST_CODE_CAPTURE_IMAGE = 100
 const val REQUEST_CODE_PICK_IMAGE = 200
 
 class ImageUtils {
@@ -15,15 +19,20 @@ class ImageUtils {
                 type = "image/*"
                 putExtra(Intent.EXTRA_ALLOW_MULTIPLE, allowMultiple)
             }
+
+        @JvmStatic
+        fun createCaptureImageIntent(photoUri: Uri) =
+            Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+                putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+            }
     }
 
 }
 
-fun Activity.dispatchPickImageIntent(intent: Intent) {
-    startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE)
-}
+private fun resolveCaptureImageResult(requestCode: Int, resultCode: Int, data: Intent?) =
+    requestCode == REQUEST_CODE_CAPTURE_IMAGE && resultCode == Activity.RESULT_OK
 
-fun Activity.resolvePickImageResult(requestCode: Int, resultCode: Int, data: Intent?): List<Uri> {
+private fun resolvePickImageResult(requestCode: Int, resultCode: Int, data: Intent?): List<Uri> {
     if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == Activity.RESULT_OK) {
         data?.data?.let {
             return listOf(it)
@@ -38,3 +47,35 @@ fun Activity.resolvePickImageResult(requestCode: Int, resultCode: Int, data: Int
     }
     return emptyList()
 }
+
+fun Activity.dispatchCaptureImageIntent(intent: Intent) =
+    intent.resolveActivity(packageManager)?.let {
+        startActivityForResult(intent, REQUEST_CODE_CAPTURE_IMAGE)
+    }
+
+fun Fragment.dispatchCaptureImageIntent(intent: Intent) =
+    intent.resolveActivity(requireContext().packageManager)?.let {
+        startActivityForResult(intent, REQUEST_CODE_CAPTURE_IMAGE)
+    }
+
+fun Activity.dispatchPickImageIntent(intent: Intent) =
+    intent.resolveActivity(packageManager)?.let {
+        startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE)
+    }
+
+fun Fragment.dispatchPickImageIntent(intent: Intent) =
+    intent.resolveActivity(requireContext().packageManager)?.let {
+        startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE)
+    }
+
+fun Activity.resolveCaptureImageResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean =
+    resolveCaptureImageResult(requestCode, resultCode, data)
+
+fun Fragment.resolveCaptureImageResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean =
+    resolveCaptureImageResult(requestCode, resultCode, data)
+
+fun Activity.resolvePickImageResult(requestCode: Int, resultCode: Int, data: Intent?): List<Uri> =
+    resolvePickImageResult(requestCode, resultCode, data)
+
+fun Fragment.resolvePickImageResult(requestCode: Int, resultCode: Int, data: Intent?): List<Uri> =
+    resolvePickImageResult(requestCode, resultCode, data)
